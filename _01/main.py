705b7396b5c3293f4132ed04927a89abbdb6ee7b
Daniel David Bador <danieldavidbador@yahoo.com>
2022-10-13 19:16:27 +0800

    added sections

--- _01/main.py
+++ _01/main.py
@@ -6,16 +6,16 @@ from conclusion import _conclusion
 
 class Presentation(Scene):
     def construct(self):
-        self.next_section(skip_animations=False)
+        self.next_section("Intro", skip_animations=False)
         _intro(self)
 
-        self.next_section(skip_animations=False)
+        self.next_section("Ratio Test", skip_animations=False)
         _ratio_test(self)
         
-        self.next_section(skip_animations=False)
+        self.next_section("Limit Test", skip_animations=False)
         _limit_test(self)
 
-        self.next_section(skip_animations=False)
+        self.next_section("Conclusion", skip_animations=False)
         _conclusion(self)
 
 if __name__ == "__main__":
