f95745c596d2022a442e1cc37019a4e835253276
Daniel David Bador <danieldavidbador@yahoo.com>
2022-10-13 20:16:30 +0800

    updated limit test animations

--- _01/limit_test.py
+++ _01/limit_test.py
@@ -8,7 +8,7 @@ def _limit_test(self):
     title = Title("Limit Comparison Test")
     self.play(Transform(limit_test, title))
     given1 = MathTex(r"u_n", r"\text{ and }", r"v_n")
-    given2 = MathTex(r"L", r"=\lim_{n \to \infty} ", r"{{u_n}", r"\over", r"{v_n}}")
+    given2 = MathTex(r"\lim_{n \to \infty} ", r"{{u_n}", r"\over", r"{v_n}}", r"=", r"L")
     conditions = VGroup(
         Tex(r"If ", r"$L$", r"$=0$ and $\sum$",r"$v_n$", r" converges, then $\sum$", r"$u_n$", r" converges"),
         Tex(r"If ", r"$L$", r"$=\infty$ and $\sum$", r"$v_n$", r" diverges, then $\sum$", r"$u_n$", r" diverges"),
@@ -17,9 +17,10 @@ def _limit_test(self):
 
     given1[0].set_color(BLUE)
     given1[2].set_color(GREEN)
-    given2[0].set_color(TEAL)
-    given2[2].set_color(BLUE)
-    given2[4].set_color(GREEN)
+    given2[1].set_color(BLUE)
+    given2[3].set_color(GREEN)
+    given2[4].set_color(TEAL)
+
     conditions[0][1].set_color(TEAL)
     conditions[0][3].set_color(GREEN)
     conditions[0][5].set_color(BLUE)
@@ -52,37 +53,37 @@ def _limit_test(self):
     given[2].set_color(BLUE)
     given[6].set_color(GREEN)
     
-    self.play(TransformMatchingTex(given1, given))
+    self.play(ReplacementTransform(given1, given))
 
     # Limit Comparison Test Solving Part
 
     limit_test_lines = VGroup(
-        MathTex(r"L", r"=\lim_{n \to \infty} ", r"{{\frac{2n}{2n!}", r"\over", r"\frac{n^3}{n!}}"),
-        MathTex(r"L", r"=\lim_{n \to \infty} ", r"{{\frac{n}{n!}", r"\over", r"\frac{n^3}{n!}}"),
-        MathTex(r"L", r"=\lim_{n \to \infty} ", r"{{n}", r"\over", r"{n^3}}"),
-        MathTex(r"L", r"=\lim_{n \to \infty} ", r"{{1}", r"\over", r"{n^2}}"),
+        MathTex(r"\lim_{n \to \infty}", r"{{u_n}", r"\over", r"{v_n}}", r"=\lim_{n \to \infty} ", r"{{\frac{2n}{2n!}", r"\over", r"\frac{n^3}{n!}}"),
+        MathTex(r"\lim_{n \to \infty}", r"{{u_n}", r"\over", r"{v_n}}", r"=\lim_{n \to \infty} { \frac{n}{n!}\over\frac{n^3}{n!} }"),
+        MathTex(r"\lim_{n \to \infty}", r"{{u_n}", r"\over", r"{v_n}}", r"=\lim_{n \to \infty} { n\over{n^3} }"),
+        MathTex(r"\lim_{n \to \infty}", r"{{u_n}", r"\over", r"{v_n}}", r"=\lim_{n \to \infty} { 1\over{n^2} }"),
+        MathTex(r"\lim_{n \to \infty}", r"{{u_n}", r"\over", r"{v_n}}", r"=0"),
         MathTex(r"L", r"=0"),
     )
 
-    limit_test_lines[0][2].set_color(BLUE)
-    limit_test_lines[0][4].set_color(GREEN)
-
-    for lines in limit_test_lines:
-        lines[0].set_color(TEAL)
+    limit_test_lines[0][1].set_color(BLUE)
+    limit_test_lines[0][3].set_color(GREEN)
+    limit_test_lines[0][5].set_color(BLUE)
+    limit_test_lines[0][7].set_color(GREEN)
+    limit_test_lines[5][0].set_color(TEAL)
 
     self.play(ReplacementTransform(VGroup(given2, given), limit_test_lines[0]))
     self.wait(1)
 
-    for i in range(1, 5):
-        self.play(TransformMatchingTex(limit_test_lines[i - 1], limit_test_lines[i], path_arc=90 * DEGREES))
+    for i in range(1, 6):
+        self.play(ReplacementTransform(limit_test_lines[i - 1], limit_test_lines[i]))
         self.wait(1)
 
     conditions = VGroup(
         Tex(r"If ", r"$L$", r"$=0$ and $\sum$",r"$v_n$", r" converges, then $\sum$", r"$u_n$", r" converges"),
         Tex(r"If ", r"$L$", r"$=\infty$ and $\sum$", r"$v_n$", r" diverges, then $\sum$", r"$u_n$", r" diverges"),
         Tex(r"If $0<$", r"$L$", r"$<\infty$, then either both series converge or both diverge"),
-    ).arrange(DOWN, MED_SMALL_BUFF).scale(.6)
-    conditions.arrange(LEFT, buff=MED_SMALL_BUFF)
+    ).arrange(DOWN, MED_SMALL_BUFF).scale(.6).next_to(limit_test_lines[5], DOWN)
 
     conditions[0][1].set_color(TEAL)
     conditions[0][3].set_color(GREEN)
@@ -92,14 +93,15 @@ def _limit_test(self):
     conditions[1][5].set_color(BLUE)
     conditions[2][1].set_color(TEAL)
     
-    self.play(limit_test_lines[4].animate.shift(2 * UP))
     self.play(Write(conditions))
     self.wait(1)
 
     self.play(Indicate(conditions[0]))
     self.wait(1)
 
-    self.play(Unwrite(limit_test_lines[4]))
-    self.play(Unwrite(conditions))
-    self.play(Unwrite(limit_test))
+    self.play(
+        Unwrite(limit_test_lines[5]),
+        Unwrite(conditions),
+        Unwrite(limit_test)
+        )
     self.wait(1)
