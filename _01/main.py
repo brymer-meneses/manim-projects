2e631e4605d6ac15a3510cfcc00105a5d455cc10
Brymer <meneses.brymer@gmail.com>
2022-10-13 16:26:32 +0800

    updates

--- _01/main.py
+++ _01/main.py
@@ -35,6 +35,7 @@ class Presentation(Scene):
         ratio_test = Tex("Ratio Test").scale(2).set_color_by_gradient(BLUE, GREEN)
 
         self.play(LaggedStart(Transform(group_name, title), Write(problem), lag_ratio=3))
+
         self.wait(1)
         self.play(LaggedStart(GrowFromCenter(brace), GrowFromCenter(converges), lag_ratio=1))
         self.wait(1)
@@ -42,7 +43,9 @@ class Presentation(Scene):
         self.wait(1)
         self.play(Transform(details, ratio_test), run_time=1.5)
         self.wait(1)
-        self.play(Unwrite(details))
+        ratio_test_section_header = Title("Ratio Test")
+        self.play(Transform(details, ratio_test_section_header), run_time=1.5)
+
         self.wait(1)
 
         # Ratio Test
@@ -70,7 +73,7 @@ class Presentation(Scene):
 
         self.play(Write(u_n))
         self.wait(1)
-        self.play(u_n.animate.move_to(2 * DOWN).scale(0.7))
+        self.play(u_n.animate.move_to(2.5 * DOWN).scale(0.7))
 
         ratio_test = VGroup(
             Text("Ratio Test").scale(0.75).set_color(BLUE_A),
@@ -89,27 +92,53 @@ class Presentation(Scene):
         )       
         self.play(Unwrite(u_n))
 
-        # Solving Part 
+        # RATIO TEST SOLVING PART
 
         ratio_test_lines = VGroup(
-            MathTex(r"\lim_{n \to \infty} \left|u_{n+1}\over{u_{n}}\right|", r"=", r"\lim_{n \to \infty}\left|\frac{\frac{2(n+1)}{2(n+1)!}}{\frac{2n}{2n!}}\right|"), # type: ignore
-            MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{2(n+1)\cdot 2n!}{2(n+1)!\cdot 2n}\right|"), # type: ignore
-            MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)\cdot 2n!}{(n+1)!\cdot 2n}\right|"), # type: ignore
-            MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)\cdot n!}{(n+1)!\cdot n}\right|"), # type: ignore
-            # MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)\cdot n!}{(n+1)\cdot n\cdot n!}\right|"), # type: ignore
-            # MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)}{(n+1)\cdot n}\right|"), # type: ignore
-            # MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{1}{n}\right|"), # type: ignore
+            VGroup( 
+                MathTex(r"\lim_{n \to \infty} \left|u_{n+1}\over{u_{n}}\right|", r"=", r"\lim_{n \to \infty}\left|\frac{\frac{2(n+1)}{2(n+1)!}}{\frac{2n}{2n!}}\right|"), # type: ignore
+                MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{2(n+1)\cdot 2n!}{2(n+1)!\cdot 2n}\right|"), # type: ignore
+                MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)\cdot 2n!}{(n+1)!\cdot 2n}\right|"), # type: ignore
+                MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)\cdot n!}{(n+1)!\cdot n}\right|"), # type: ignore
+            ),
+            VGroup( 
+                MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)\cdot n!}{(n+1)\cdot n\cdot n!}\right|"), # type: ignore
+                MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{(n+1)}{(n+1)\cdot n}\right|"), # type: ignore
+                MathTex(r"=", r"\lim_{n \to \infty}\left|\frac{1}{n}\right|"), # type: ignore
+            )
         )
-        ratio_test_lines.arrange(DOWN,MED_SMALL_BUFF)
-
-        ratio_test_lines[0].set_color_by_tex_to_color_map({"{u_{n+1}}": RED})
 
 
-        for line in ratio_test_lines:
-            self.play(Write(line))
+        ratio_test_lines.scale(0.8)
+        ratio_test_lines[0].arrange(DOWN, MED_SMALL_BUFF)
+        ratio_test_lines[1].arrange(DOWN, MED_SMALL_BUFF)
 
-        for line in ratio_test_lines:
-            self.play(Unwrite(line))
+        self.play(Write(ratio_test_lines[0][0]))
+        self.wait(2)
+        self.play(
+            TransformMatchingTex(ratio_test_lines[0][0].copy(), ratio_test_lines[0][1], path_arc=90 * DEGREES)
+        )
+        self.wait(2)
+        self.play(
+            TransformMatchingTex(ratio_test_lines[0][1].copy(), ratio_test_lines[0][2], path_arc=90 * DEGREES)
+        )
+        self.wait(2)
+        self.play(
+            TransformMatchingTex(ratio_test_lines[0][2].copy(), ratio_test_lines[0][3], path_arc=90 * DEGREES)
+        )
+        self.wait(2)
+        # self.play(
+        #     TransformMatchingTex(lines[2].copy(), lines[3], path_arc=90 * DEGREES)
+        # )
+        # self.wait(2)
+        # self.play(
+        #     TransformMatchingTex(lines[3].copy(), lines[4], path_arc=90 * DEGREES)
+        # )
+
+        self.wait(5)
+        self.play(Unwrite(ratio_test_lines))
+
+        self.play(Unwrite(details))
 
         # Limit Comparison Test
 
