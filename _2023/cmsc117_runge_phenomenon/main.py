b329bb990856165eb50e6c921ed987629c600e0b
Daniel David N. Bador <danieldavidbador@yahoo.com>
2023-11-24 23:43:15 +0800

    Refactor Runge Phenomenon presentation

--- _2023/cmsc117_runge_phenomenon/main.py
+++ _2023/cmsc117_runge_phenomenon/main.py
@@ -1,8 +1,9 @@
 import numpy as np
-import polyinterp as pi
 
 from manim import *
 from manim_slides import Slide
+
+from polyinterp import NewtonLagrangeInterpolation
 
 class Presentation(Slide):
   def construct(self):
@@ -11,42 +12,51 @@ class Presentation(Slide):
     self.play_newton_lagrange_interpolation()
 
   def play_blank_slide(self):
-    self.wait(1)
+    self.wait(0.1)
     self.next_slide()
 
   def play_introduction(self):
     text = Text("Runge Phenomenon").set_color_by_gradient(BLUE, GREEN).scale(1.5)
-    self.play(Write(text))
-    self.wait(1)
+
+    self.play(Write(text), run_time=0.5)
+    self.wait(0.1)
 
     self.next_slide()
-    self.play(Unwrite(text))
-    self.wait(1)
+    self.play(Unwrite(text), run_time=0.5)
+    self.wait(0.1)
 
   def play_newton_lagrange_interpolation(self):
     axes = Axes(x_range=[-10, 10])
-    func_graph = axes.plot(lambda x: np.sin(x), color=BLUE)
+    f = lambda x: np.sin(x)
+    f_graph = axes.plot(f, color=BLUE)
 
-    self.play(LaggedStart(Create(axes), Create(func_graph), lag_ratio=0.5))
-    self.wait(1)
-
-    self.next_slide()
-    approx_graph = axes.plot(
-      pi.NewtonLagrangeInterpolation(
-        lambda x: np.sin(x), np.linspace(-10, 10, 10)
-      ),
-      color=GREEN,
-    )
-    self.play(LaggedStart(Create(approx_graph), lag_ratio=0.5))
-    self.wait(1)
+    self.play(Create(axes), Create(f_graph, run_time=1))
+    self.wait(0.1)
 
     self.next_slide()
+    for n in [5, 10, 15, 20, 25]:
+      nodes = np.linspace(-10, 10, n)
+      points = VGroup(*[Dot(axes.coords_to_point(x, f(x))) for x in nodes])
+      approx_graph = axes.plot(
+        NewtonLagrangeInterpolation(f, nodes),
+        color=GREEN,
+      )
+
+      self.play(Succession(Create(points), Create(approx_graph)))
+      self.wait(0.1)
+
+      self.next_slide()
+      self.play(
+        FadeOut(points),
+        Uncreate(approx_graph),
+      )
+      self.wait(1)
+    
     self.play(
       LaggedStart(
-        Uncreate(approx_graph),
-        Uncreate(func_graph),
+        Uncreate(f_graph),
         Uncreate(axes),
         lag_ratio=0.5,
       )
     )
-    self.wait(1)
+    self.wait(0.1)
